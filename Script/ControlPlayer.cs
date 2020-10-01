dca21453349647fab124d524ea12f08df7a35031
Muhammad Abbyu J <71117074+mabbyu@users.noreply.github.com>
2020-10-01 20:34:33 +0700

    Add files via upload

---
+++ Script/ControlPlayer.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlPlayer : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+    private double count;
+    public Text countText;
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up Gold"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 10;
+            SetCountText();
+        }
+        if (other.gameObject.CompareTag("Pick Up Silver"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 8;
+            SetCountText();
+        }
+        if (other.gameObject.CompareTag("Pick Up Bronze"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 2;
+            SetCountText();
+        }
+        if (other.gameObject.CompareTag("Pick Up Gold"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 10;
+            SetCountText();
+        }
+        if (other.gameObject.CompareTag("Pick Up Silver"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 8;
+            SetCountText();
+        }
+        if (other.gameObject.CompareTag("Pick Up Zonk"))
+        {
+            gameObject.SetActive(false);
+            count = count;
+            SetCountText();
+        }
+    }
+    
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+}
---
+++ Script/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+
+    void Update()
+    {
+        print (Time.deltaTime);
+        transform.Rotate(new Vector3(50, 0, 0) * Time.deltaTime);
+    }
+}
