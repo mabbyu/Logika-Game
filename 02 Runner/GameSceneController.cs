0c43450c341ffe4769f18ab7d775801a672a6981
Muhammad Abbyu J <71117074+mabbyu@users.noreply.github.com>
2020-10-05 23:52:12 +0700

    Add files via upload

---
+++ 02 Runner/GameSceneController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSceneController : MonoBehaviour
+{
+
+    public PlayerController player;
+    public Camera gameCamera;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        gameCamera.transform.position = new Vector3(player.transform.position.x, gameCamera.transform.position.y, gameCamera.transform.position.z);
+    }
+}
---
+++ 02 Runner/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Runtime.InteropServices;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public float jumpForce;
+    public float horizontalVelocity;
+    private int count;
+    public Text countText;
+
+    void SetCountText()
+    {
+        countText.text = " Point : " + count.ToString();
+    }
+
+    void Start()
+    {
+        count = 0;
+        SetCountText();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            this.GetComponent<Rigidbody>().AddForce(new Vector3(0, jumpForce, 0));
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 10;
+            SetCountText();
+        }
+        if (other.gameObject.CompareTag("Trap"))
+        {
+            gameObject.SetActive(false);
+            count = count;
+            SetCountText();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Jump"))
+            print("horizontalVelocity : " + horizontalVelocity + "; Time.deltaTime : " + Time.deltaTime + " >>x : " + horizontalVelocity * Time.deltaTime);
+            this.GetComponent<Rigidbody>().velocity = new Vector3(horizontalVelocity * Time.deltaTime, this.GetComponent<Rigidbody>().velocity.y, this.GetComponent<Rigidbody>().velocity.z);
+    }
+}
