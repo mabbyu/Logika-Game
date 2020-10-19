ef6398e68aefe156374e8e124619e275976a1213
Muhammad Abbyu J <71117074+mabbyu@users.noreply.github.com>
2020-10-19 20:49:15 +0700

    Update GameSceneController.cs

--- 02 Runner/GameSceneController.cs
+++ 02 Runner/GameSceneController.cs
@@ -1,20 +1,49 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameSceneController : MonoBehaviour
 {
 
     public PlayerController player;
+    public GameObject[] blockPrefab;
     public Camera gameCamera;
+    public Text gameText;
+    private float blockPointer = -10;
+    private float safeMargin = 20;
 
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        gameCamera.transform.position = new Vector3(player.transform.position.x, gameCamera.transform.position.y, gameCamera.transform.position.z);
+        if (player != null)
+        {
+            while (player != null && blockPointer < player.transform.position.x + safeMargin)
+            {
+                int blockIndex = Random.Range(0, blockPrefab.Length);
+                if (blockPointer < 20)
+                    blockIndex = 0;
+                GameObject blockObject = GameObject.Instantiate<GameObject>(blockPrefab[blockIndex]);
+                blockObject.transform.SetParent(this.transform);
+                BlockController block = blockObject.GetComponent<BlockController>();
+                blockObject.transform.position = new Vector3(blockPointer + block.size / 2, 0, 0);
+                blockPointer += block.size;
+            }
+            gameCamera.transform.position = new Vector3(player.transform.position.x, gameCamera.transform.position.y, gameCamera.transform.position.z);
+            gameText.text = " Score : " + Mathf.Floor(player.transform.position.x);
+        }
+        if (player == null)
+        {
+            gameText.text += "\nPress R to restart";
+        }
+        if (Input.GetKeyDown("r"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 }
